1810d7c8ad73df3c1043f891bc0e46806c5f45a1
Eugene Khristo <marteau9292@gmail.com>
2025-04-28 09:51:15 +0300

    Styled comonents Intro

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import styled from 'styled-components';
+
+const H1 = styled.h1`
+  font-size: 40px;
+  font-weight: 900;
+  background-color: orangered;
+  color: white;
+`;
+
+const Button = styled.button`
+  background-color: darkviolet;
+  color: white;
+  text-transform: uppercase;
+  font-family: sans-serif;
+  padding: 1.2rem 2rem;
+  border-radius: 20px;
+  cursor: pointer;
+  margin: 20px;
+  border: none;
+  font-weight: 600;
+  font-size: 20px;
+`;
+
+const Input = styled.input`
+  border: 1px solid #eee;
+  border-radius: 16px;
+  padding: 0.8rem 1.2rem;
+  outline: none;
+`;
+
+const StyledApp = styled.div`
+  background-color: palegoldenrod;
+  padding: 32px;
+`;
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <StyledApp>
+      <H1>Hello Oasis</H1>
+      <Button onClick={() => alert('Checking in...')}>Check in</Button>
+      <Button onClick={() => alert('Checking out...')}>Check out</Button>
+      <Input placeholder="Sosi" defaultValue={'WTF MA FREN? 🔥'} />
+    </StyledApp>
+  );
 }
 
-export default App
+export default App;
