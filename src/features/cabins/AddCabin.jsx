13c80cd5015e5a4866022009f313c96e0d3d35fc
Eugene Khristo <marteau9292@gmail.com>
2025-05-05 21:56:20 +0300

    Compound Component pattern applied to Modal

--- src/features/cabins/AddCabin.jsx
+++ src/features/cabins/AddCabin.jsx
@@ -1,23 +1,17 @@
-import { useState } from 'react';
 import Button from '../../ui/Button';
 import Modal from '../../ui/Modal';
 import CreateCabinForm from './CreateCabinForm';
 
 function AddCabin() {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
   return (
-    <div>
-      <Button onClick={() => setIsModalOpen((isModalOpen) => !isModalOpen)}>
-        Add new cabin
-      </Button>
-
-      {isModalOpen && (
-        <Modal onCloseModal={() => setIsModalOpen(false)}>
-          <CreateCabinForm onCloseModal={() => setIsModalOpen(false)} />
-        </Modal>
-      )}
-    </div>
+    <Modal>
+      <Modal.Open opens="cabin-form">
+        <Button>Add new cabin</Button>
+      </Modal.Open>
+      <Modal.Window name="cabin-form">
+        <CreateCabinForm />
+      </Modal.Window>
+    </Modal>
   );
 }
 
